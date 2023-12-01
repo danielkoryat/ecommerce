25200f89d26f83e944ccb324b1107e452206907a
daniel <dan.koryat@gmail.com>
2023-12-01 17:46:56 +0200

    updated basic custome middlewares

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,18 @@ env.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import corsOptionsConfig from "./config/corsOptions.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import notFound from "./middlewares/notFound.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptionsConfig));
+app.use(express.urlencoded({ extended: true }));
+app.use(notFound);
+app.use(errorHandler);
 
 const start = async () => {
     try {
